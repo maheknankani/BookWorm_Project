9035fa32feca28283161950bc1ce6702ab0eb708
Mehik-Nankani <bcsbs2212151@szabist.edu.pk>
2026-08-20 11:10:55 -0700

    Fix launch component registration and update app display name to BookWorm

--- Mobile/shims/expo-document-picker.js
+++ Mobile/shims/expo-document-picker.js
@@ -1,26 +1,4 @@
-import DocumentPicker from "react-native-document-picker";
+import * as DocumentPicker from "expo-document-picker";
 
-export const getDocumentAsync = async () => {
-  try {
-    const res = await DocumentPicker.pickSingle({
-      type: [DocumentPicker.types.pdf],
-    });
-    return {
-      type: "success",
-      canceled: false,
-      assets: [
-        {
-          uri: res.uri,
-          name: res.name,
-          size: res.size,
-          mimeType: res.type,
-        },
-      ],
-    };
-  } catch (err) {
-    if (DocumentPicker.isCancel(err)) {
-      return { canceled: true, type: "cancel" };
-    }
-    throw err;
-  }
-};
+export const getDocumentAsync = DocumentPicker.getDocumentAsync;
+export default DocumentPicker;
